518bcf29dabcbc6f3d45b775b2ba7fb854f92e9a
Rita-cedcoss <ritapal@cedcoss.com>
2022-12-02 18:55:00 +0530

    product data list data

--- src/Product_List/ProductListApp.js
+++ src/Product_List/ProductListApp.js
@@ -19,6 +19,17 @@ console.log(unique);
   useEffect(() => {
     checkarrData();
   }, []);
+//   List the products in drop-down from the products data provided in attached text file.
+// Steps:
+// Access JSON data provided and list all the unique product_category in first drop-down.
+// On selecting a product category created in step 1 show category_1 of selected category in another drop-down.
+// Selecting category_1 show category_2 in another drop-down if exists else show "no data".
+// Further selecting category_2 show category_3 in a span if exists else show "no data".
+// Save the data to local storage using state and display in a table.
+// Use proper coding style, avoid unnecessary re-rendering and code re-usability where ever applicable.   
+// Sample is attached for reference:  
+
+//combine reducer
   // make three reducers (one for cake and one for ice-cream and one for chocolates  ) and combine these reducers in one reducer and make store of it.
   // take order from user and subtract the order from the available quantity. 
   // eg. user wants to buy 4 chocolates and 2 cakes then if the product is in stock then subtract the quantity .
